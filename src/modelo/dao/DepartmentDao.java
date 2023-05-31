d088d7be0962cfde72d1cf0bd8b5ea9103854bba
ulisses-araujo <ulissesdeveloper82@gmail.com>
2023-05-31 16:37:51 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/modelo/dao/DepartmentDao.java
@@ -0,0 +1,13 @@
+package modelo.dao;
+
+import java.util.List;
+
+import modelo.entities.Department;
+
+public interface DepartmentDao {
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+}
