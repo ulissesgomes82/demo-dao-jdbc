19bcb86e69cf89ab76ff324eda4bd2f11fdeba3b
ulisses-araujo <ulissesdeveloper82@gmail.com>
2023-05-31 15:54:34 -0300

    Creating project and git repository

---
+++ src/db/DB.java
@@ -0,0 +1,70 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+	private static Connection connection = null;
+
+	public static Connection getConnection() {
+		if (connection == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				connection = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
+
+	public static void closeConnection() {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+
+	}
+
+	public static void closePreparedStatement(PreparedStatement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
